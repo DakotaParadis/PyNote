9c578e35b3ac2dbc42d93b2f7609d018b60e08fb
DakotaParadis <dakotaparadis1@gmail.com>
2022-09-13 21:16:22 -0400

    First commit for PyNotes

--- Notes.py
+++ Notes.py
@@ -1,1 +1,106 @@
-# This is a test to get the repository started
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+class App(customtkinter.CTk):
+    WIDTH=1920
+    HEIGHT=1080
+
+    def __init__(self):
+        super().__init__()
+    
+        self.title("PyNote by Dakota Paradis") #Applciation window title
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="PyNote",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left, #Add Note button
+                                                text="AddNote",
+                                                command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_left, #Delete note button
+                                                text="Delete Note",
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left, #Settings note button
+                                                text="Settings",
+                                                command=self.button_event)
+        self.button_3.grid(row=4, column=0, pady=10, padx=10)
+
+        #self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        #self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        #self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                        #values=["Light", "Dark"],
+                                                        #command=self.change_appearance_mode)
+        #self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+        # ============ frame_right ============
+
+        # configure grid layout (3x7)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_right.rowconfigure(7, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+        self.frame_info.rowconfigure(0, weight=1)
+        self.frame_info.columnconfigure(0, weight=1)
+
+        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+                                                   text="CTkLabel: Lorem ipsum dolor sit,\n" +
+                                                        "amet consetetur sadipscing elitr,\n" +
+                                                        "sed diam nonumy eirmod tempor" ,
+                                                   height=100,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT)
+        self.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+        # set default values
+        #self.optionmenu_1.set("Dark")
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+    
+    def on_closing(self, event=0):
+        self.destroy()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
